96ac9f6d66558d1b3df5b5246376753e57685cdf
Meetali Kapse <meetalikapse@gmail.com>
2024-04-29 20:38:55 +0530

    Service worker works done

---
+++ Desktop/Quiz_App-main/public/sw1.js
@@ -0,0 +1,17 @@
+var cacheurl = ["index.html"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("MyCacheFile").then((cache) => {
+      return cache.addAll(cacheurl);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request)).then((res) => {
+    if (res) {
+      return res;
+    }
+    return fetch(event.request);
+  });
+});
--- Desktop/Quiz_App-main/src/index.js
+++ Desktop/Quiz_App-main/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom"; // Use BrowserRouter from react-router-dom
+import { BrowserRouter as Router } from "react-router-dom";
 import Quiz from "./Component/Quiz";
 
 ReactDOM.render(
@@ -9,13 +9,4 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.log("Service Worker registration failed:", error);
-    });
-}
+
